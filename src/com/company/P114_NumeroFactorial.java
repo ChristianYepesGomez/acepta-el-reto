e560cee8ebd0bbb7c1a0b2f3818aed1a9a351899
Christian Yepes Gomez <christianyepesgomez@gmail.com>
2021-12-07 16:13:25 +0100

    start problem

--- src/com/company/P114_NumeroFactorial.java
+++ src/com/company/P114_NumeroFactorial.java
@@ -2,10 +2,14 @@ import java.util.Scanner;
 
 public class P114_NumeroFactorial {
 
-	public static void main(String[] args) {
-		//	TODO
-	
-		
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int ncasos = sc.nextInt();
+
+        for (int i = 0; i <; i++) {
+            
+        }
+    }
 
 }
