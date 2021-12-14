295f7dde70c7dc2cbbf16bb8afc2583e7ecbeb62
Christian Yepes Gomez <christianyepesgomez@gmail.com>
2021-12-14 22:27:20 +0100

    P139 DONE

--- AER/src/com/company/P139_NumerosCubinfinitos.java
+++ AER/src/com/company/P139_NumerosCubinfinitos.java
@@ -1,48 +1,61 @@
 package com.company;
 
 import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class P139_NumerosCubinfinitos {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        String cuboString, cuboStringAux, res;
-        int suma;
+        String cuboString;
+        int suma, cubo, digito;
+        Set<Integer> set;
         do {
 
-            cuboString = sc.next();
-            suma = 0;
-            res = cuboString + " - ";
-            if (!cuboString.equals("0")) {
-                if (cuboString.equals("1")) {
-                    System.out.println("1 -> cubinfinito.");
+            cubo = sc.nextInt();
+
+            if (cubo != 0) {
+                suma = 0;
+
+                set = new TreeSet<>();
+                set.add(cubo);
+
+                if (cubo == 1) {
+                    System.out.println("1 -> cubifinito.");
                 } else {
-                    while (suma != 1) {
-
+                    System.out.print(cubo + " - ");
+                    while (true) {
+                        cuboString = Integer.toString(cubo);
                         for (int i = 0; i < cuboString.length(); i++) {
-
-                            suma += Math.pow(Integer.parseInt(cuboString.substring(i, i + 1)), 3);
+                            digito = Integer.parseInt("" + cuboString.charAt(i));
+                            suma += digito * digito * digito;
 
                         }
 
-                        cuboString = Integer.toString(suma);
+                        cubo = suma;
 
                         if (suma == 1) {
-                            res += "1 -> cubinfinito.";
+                            System.out.println("1 -> cubifinito.");
+                            break;
+
+                        } else if (set.contains(suma)) {
+                            System.out.println(cubo + " -> no cubifinito.");
+                            break;
 
                         } else {
-                            res += cuboString + " - ";
+                            set.add(suma);
+                            System.out.print(cubo + " - ");
                             suma = 0;
                         }
                     }
-                    System.out.println(res);
                 }
 
 
             }
 
-        } while (!cuboString.equals("0"));
+        } while (cubo != 0);
     }
 
 }
